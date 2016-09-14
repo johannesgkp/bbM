80cb28035f9f31d17f2d42133226c84ee5dbfc07
johannesgkp <flowyoe@gmail.com>
2016-09-14 13:50:33 +0200

    oop

---
+++ src/game.js
@@ -0,0 +1,23 @@
+/*
+* Constructor for game
+* @param
+*/
+function Game() {
+	this.player = new Array;
+	// fighters from the player
+	this.fighterArray = new Array;
+	this.buyableFighter = new Array;
+	// neccesary for gui
+	this.numberOfFightingFighter = 0;
+	this.table = document.getElementById("announcements");
+	
+	{
+}
+
+/*
+* 
+* @param 
+*/
+Game.prototype.drinking = function(time) {
+	
+};
